4927c76f88a04c8b4984c776c5b81c08ebc05abe
ein <ein@hpnull2>
2019-04-08 00:40:35 +0900

    fix rsync script.

--- bin/_remove-old-files.sh
+++ bin/_remove-old-files.sh
@@ -3,11 +3,13 @@
 GIT_REPO_ROOT_DIRECTORY=`git rev-parse --show-toplevel`
 GIT_REPO_DESTINATION_DIRECTORY="$GIT_REPO_ROOT_DIRECTORY/../nobiruwa.github.io.master.git/"
 
-rsync -a --filter='P $GIT_REPO_ROOT_DIRECTORY/_site/'      \
-         --filter='P $GIT_REPO_ROOT_DIRECTORY/_cache/'     \
-         --filter='P $GIT_REPO_ROOT_DIRECTORY/.git/'       \
-         --filter='P $GIT_REPO_ROOT_DIRECTORY/.gitignore'  \
-         --filter='P $GIT_REPO_ROOT_DIRECTORY/.stack-work' \
-         --delete-excluded        \
+rsync -a --filter='P $GIT_REPO_DESTINATION_DIRECTORY/_site/'      \
+         --filter='P $GIT_REPO_DESTINATION_DIRECTORY/_cache/'     \
+         --filter='P $GIT_REPO_DESTINATION_DIRECTORY/.git/'       \
+         --filter='P $GIT_REPO_DESTINATION_DIRECTORY/.gitignore'  \
+         --filter='P $GIT_REPO_DESTINATION_DIRECTORY/.stack-work' \
+         --exclude=".git" \
+         --exclude=".gitignore" \
+         --delete \
          "$GIT_REPO_ROOT_DIRECTORY/_site/" \
          "$GIT_REPO_DESTINATION_DIRECTORY"
