509a6a109a792515b456b6108bcb8cf04f98366b
ein@letsrei.localdomain <ein@letsrei.localdomain>
2023-04-08 14:52:02 +0900

    updated.

--- site.hs
+++ site.hs
@@ -1,97 +1,99 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import Data.Monoid (mappend)
-import Hakyll
-    ( Context,
-      applyAsTemplate,
-      compile,
-      compressCssCompiler,
-      constField,
-      copyFileCompiler,
-      create,
-      dateField,
-      defaultContext,
-      fromList,
-      getResourceBody,
-      hakyll,
-      idRoute,
-      listField,
-      loadAll,
-      loadAndApplyTemplate,
-      makeItem,
-      match,
-      pandocCompiler,
-      recentFirst,
-      relativizeUrls,
-      route,
-      setExtension,
-      templateCompiler )
+
+import Hakyll (
+  Context,
+  applyAsTemplate,
+  compile,
+  compressCssCompiler,
+  constField,
+  copyFileCompiler,
+  create,
+  dateField,
+  defaultContext,
+  fromList,
+  getResourceBody,
+  hakyll,
+  idRoute,
+  listField,
+  loadAll,
+  loadAndApplyTemplate,
+  makeItem,
+  match,
+  pandocCompiler,
+  recentFirst,
+  relativizeUrls,
+  route,
+  setExtension,
+  templateCompiler,
+ )
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "favicon.ico" $ do
-      route idRoute
-      compile copyFileCompiler
+  match "favicon.ico" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match "css/*/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+  match "css/*/*" $ do
+    route idRoute
+    compile compressCssCompiler
 
-    match (fromList [{- "about.rst", "contact.md" -}]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
+  match (fromList [] {- "about.rst", "contact.md" -}) $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        >>= relativizeUrls
 
-    match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile $
+      pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html" postCtx
+        >>= loadAndApplyTemplate "templates/default.html" postCtx
+        >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "過去の投稿"            `mappend`
-                    defaultContext
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "過去の投稿"
+              `mappend` defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Home"
+              `mappend` defaultContext
 
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
 
-    match "templates/*" $ compile templateCompiler
+  match "templates/*" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
-
+  dateField "date" "%B %e, %Y"
+    `mappend` defaultContext
